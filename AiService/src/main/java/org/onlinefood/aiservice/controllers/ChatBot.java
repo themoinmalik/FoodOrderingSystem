4259a42ee9b1222dad761cdb28d0d6a974a5c38a
Moin Malik <moimalik@deloitte.com>
2026-03-09 10:23:14 +0530

    ai chat bot feature integartion started

---
+++ AiService/src/main/java/org/onlinefood/aiservice/controllers/ChatBot.java
@@ -0,0 +1,14 @@
+package org.onlinefood.aiservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatBot {
+
+
+    @GetMapping("/askQuestion")
+    public void askQuestion(){
+
+    }
+}
